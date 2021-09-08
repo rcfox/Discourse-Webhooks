63250bda6babe7ba586789098cbb9b05ecac76f5
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2021-09-08 13:27:47 -0400

    test

--- plugin.rb
+++ plugin.rb
@@ -96,11 +96,11 @@ after_initialize do
         topic_uri = URI.parse("#{site_url}t/#{topic_id}.json")
         # topic_uri.query = ({:api_key => SiteSetting.webhooks_discourse_api_key, :api_username => SiteSetting.webhooks_discourse_api_username})
         topic_http = Net::HTTP.new(topic_uri.host, topic_uri.port)
-        topic_http["Api-Key"] = SiteSetting.webhooks_discourse_api_key; 
-        topic_http["Api-Username"] = SiteSetting.webhooks_discourse_api_username; 
         topic_http.use_ssl = true if topic_uri.scheme == 'https'
         topic_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         topic_request = Net::HTTP::Get.new(topic_uri)
+        topic_request["Api-Key"] = SiteSetting.webhooks_discourse_api_key; 
+        topic_request["Api-Username"] = SiteSetting.webhooks_discourse_api_username; 
 
         # Send topic request
         Rails.logger.debug("Getting topic from: #{topic_uri.to_s}")
