8cf470d735f441c6dc6fecd61897c25422167379
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2021-09-08 16:50:00 -0400

    final commit

--- plugin.rb
+++ plugin.rb
@@ -94,7 +94,6 @@ after_initialize do
 
         # Configure topic request
         topic_uri = URI.parse("#{site_url}t/#{topic_id}.json")
-        # topic_uri.query = URI.encode_www_form({:api_key => SiteSetting.webhooks_discourse_api_key, :api_username => SiteSetting.webhooks_discourse_api_username})
         topic_http = Net::HTTP.new(topic_uri.host, topic_uri.port)
         topic_http.use_ssl = true if topic_uri.scheme == 'https'
         topic_http.verify_mode = OpenSSL::SSL::VERIFY_NONE
